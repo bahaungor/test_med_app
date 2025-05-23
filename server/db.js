eca172f5bc452a284ea68de38845e024877040ec
bahaungor <bahadirungor@gmail.com>
2025-05-23 09:47:05 -0400

    db imp

--- server/db.js
+++ server/db.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const mongoURI =  "mongodb://root:<your-password>@127.0.0.1:27017";
+const mongoURI =  "mongodb://root:tGtuZdqVImEDUwRYmHTLEH7A@172.21.199.110:27017";
 
 const connectToMongo = async (retryCount) => {
     const MAX_RETRIES = 3;
